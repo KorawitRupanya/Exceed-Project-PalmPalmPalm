3a1760cb8eb0336f2c039a10133de66c194c7b9d
KameriiJ <jenny.sirarat@gmail.com>
2018-07-21 23:54:52 +0700

    modified home page

--- home-app.js
+++ home-app.js
@@ -1,37 +1,35 @@
 $(function () {
-    let image = "ผล.png"
-    $("#buttomsmall").on("click", function () {
-        image = 'ผล.jpg'
+    let image = "png/big-palm-icon.png"
+    let id = "big-palm-icon"
+    $("#big-palm-button").on("click", function () {
+        image = 'png/big-palm-icon.png'
+        id = "big-palm-icon"
     })
 
 
-    $("#buttommid").on("click", function () {
-        image = 'a.png'
-    })
-
-
-    $("#buttomlarge").on("click", function () {
-        image = 'b.png'
+    $("#small-palm-button").on("click", function () {
+        image = 'png/palm2.png'
+        id = "small-palm-icon"
     })
 
     $("#savepic").on("click", function () {
         let name = $("#namepic").val();
-        let descip = $("#descip").val();
+        let descript = $("#descript").val();
         $("#box").append(`
             <div class="card" style="width: 16rem; margin-top:30px; margin-left: auto; margin-right: auto;">
-            <img class="card-img-top" src="${image}" alt="">
+            <img id="${id}" class="card-img-top" src="${image}" alt="">
             <div class="card-body">
                 <p class="card-text">
                     <b>Name:</b> ${name}<hr>
-                    <b>Description:</b> ${descip}
+                    <b>Description:</b> ${descript}
                 </p>
                 <a href="profile.html"><button>Profile</button></a>
             </div>
             </div> 
         `)
-         name = $("#namepic").val("");
-         descip = $("#descip").val("");
-        
+        name = $("#namepic").val("");
+        descip = $("#descip").val("");
+
     })
 })
 
