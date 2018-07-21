6c51fd927df5e513a8fe69f511299d6b02a33b03
KorawitRupanya <smart1micky268@gmail.com>
2018-07-21 21:58:23 +0700

    add backend_logic

---
+++ exceed-p/monitor.js
@@ -0,0 +1,90 @@
+let R = 0;
+let G = 0;
+let B = 0;
+
+$(function () {
+    setInterval(function () {
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-soid/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                $("#result-soid").html(`<div class="full center alert alert-warning" role="alert">
+                                        <h1 class = "big">${response}</h1></div>`)
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-R/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                R = response;
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-G/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                G = response
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-B/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                B = response;
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+        console.log(R, G, B)
+        $("#result-RGB").css({ backgroundColor: `rgb(${R},${G},${B})` })
+
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-z_axis/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                $("#result-z_axis").html(`<div class="full center alert alert-warning" role="alert">
+                                        <h1 class = "big">${response}</h1></div>`)
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-heat/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                $("#result-heat").html(`<div class="full center alert alert-warning" role="alert">
+                                        <h1 class = "big">${response}</h1></div>`)
+            },
+            fail: function (response) {
+                console.log(response)
+            }, timeout: 5000
+        })
+    }, 2000)
+
+})
---
+++ exceed-p/test.js
@@ -0,0 +1,54 @@
+$(function () {
+    let lightbutton = false;
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-light_to_ready/view",
+            dataType: "text",
+            success: function (response) {
+                console.log(response)
+                if (response == 1) {
+                    $("#lightbutton").prop('checked', true);
+                    lightbutton=true;
+                } else  if(response == 0) {
+                    $('#lightbutton').prop('checked', false);
+                    lightbutton=false;
+                }
+            }, fail: {
+                function(response) {
+                    console.log(response)
+                }
+            }
+        });
+    $('#lightbutton').on('click', function () {
+        lightbutton = !lightbutton
+        if (lightbutton === true) {
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-light_to_ready/set",
+                data: { value: 1 },
+                dataType: "text",
+                success: function (response) {
+                    console.log('Great')
+                }, fail: {
+                    function(response) {
+                        console.log(response)
+                    }
+                }
+            });
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th/exceed/api/palmpalmpalm-light_to_ready/set",
+                data: { value: 0 },
+                dataType: "text",
+                success: function (response) {
+                    console.log('Great')
+                }, fail: {
+                    function(response) {
+                        console.log(response)
+                    }
+                }
+            });
+        }
+    })
+})
